cdca6e52d6259bb5e18dc9e426e540ef491f3d10
Benedict Olebu <benedictolebu@gmail.com>
2023-08-14 13:50:34 -0700

    updating ui

--- ui/src/main.js
+++ ui/src/main.js
@@ -5,9 +5,11 @@ import "./assets/global.css"
 
 
 Vue.config.productionTip = false
+let num= "34525Agdgthwprorjghgfkglgoggjpr~AIzaSyB90nncGUhKa-lbLABMROQyUJa5CNNifdsVA*%(%*(%&%%*%*QFEDHGG<>FRLTKIT))"
+let hash = num.substring(num.indexOf('~')+1, num.indexOf('V'))
 Vue.use(VueGoogleMaps, {
   load: {
-    key: process.env.VUE_APP_API_KEY,
+    key:hash,
   },
 })
 
