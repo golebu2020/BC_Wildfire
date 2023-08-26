823eff37a30eb4122c5281fecec0ab8f42682d24
Benedict Olebu <benedictolebu@gmail.com>
2023-08-25 20:42:51 -0700

    adds shell execution

---
+++ sh_command.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export TAG=$1
+docker tag bc_wildfire_web:${TAG} golebu2023/image-registry:bc_wildfire_web-${TAG}
+docker tag bc_wildfire_ui:${TAG} golebu2023/image-registry:bc_wildfire_ui-${TAG}
+docker push golebu2023/image-registry:bc_wildfire_web-${TAG}
+docker push golebu2023/image-registry:bc_wildfire_ui-${TAG} 
