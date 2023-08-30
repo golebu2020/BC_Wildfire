c4989196dc771f2373884a86709512832941eeb5
Benedict Olebu <benedictolebu@gmail.com>
2023-08-29 17:00:43 -0700

    modified build_push.sh

--- build_push.sh
+++ build_push.sh
@@ -2,6 +2,7 @@
 
 export TAG=$1
 docker-compose build
+echo "The value of tag is ${TAG}"
 # docker tag bc_wildfire_web:${TAG} ${WEB_REG}-${TAG} 
 # docker tag bc_wildfire_ui:${TAG} ${UI_REG}-${TAG} 
 # docker push ${WEB_REG}-${TAG} 
