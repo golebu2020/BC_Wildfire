00a736e23f60b7fd8a2fedeba1556d2770ac7e8c
Benedict Olebu <benedictolebu@gmail.com>
2023-08-29 19:20:25 -0700

    modifies Jenkinsfile

--- script.groovy
+++ script.groovy
@@ -14,9 +14,9 @@ def incrementVersion(){
 }
 
 def buildPush(){
-    echo "${major}"
-    echo "${minor}"
-    echo "${patch}"
+    echo "${env.major}"
+    echo "${env.minor}"
+    echo "${env.patch}"
     // sh "docker-compose build --build-arg TAG=${TAG}"
     // sh " docker-compose run web sh -c 'python manage.py wait_for_db && python manage.py test --build-arg TAG=1.0.0' "
 }
