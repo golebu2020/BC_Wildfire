8a3b9134820ac3e96a880334937a905f7857b96d
Benedict Olebu <benedictolebu@gmail.com>
2023-08-29 17:34:09 -0700

    modifies test_and_linting.sh

--- test_and_linting.sh
+++ test_and_linting.sh
@@ -1,2 +1,2 @@
 export TAG=$1
-sh " docker-compose run web sh -c 'python manage.py wait_for_db && python manage.py test' " 
+docker-compose run web sh -c 'python manage.py wait_for_db && python manage.py test'
