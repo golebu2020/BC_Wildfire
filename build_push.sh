13f0d5da51eecc80071eaea52c73757cc0ce9e0f
Benedict Olebu <benedictolebu@gmail.com>
2023-08-29 17:15:30 -0700

    modifies Jenkinsfile and build_push.sh

--- build_push.sh
+++ build_push.sh
@@ -2,10 +2,8 @@
 
 export TAG=$1
 docker-compose build --build-arg TAG=${TAG}
-# docker-compose build
-echo "The value of tag is ${TAG}"
-# docker tag bc_wildfire_web:${TAG} ${WEB_REG}-${TAG} 
-# docker tag bc_wildfire_ui:${TAG} ${UI_REG}-${TAG} 
-# docker push ${WEB_REG}-${TAG} 
-# docker push ${UI_REG}-${TAG} 
-# docker rmi bc_wildfire_web:${TAG} bc_wildfire_ui:${TAG}
+docker tag bc_wildfire_web:${TAG} ${WEB_REG}-${TAG} 
+docker tag bc_wildfire_ui:${TAG} ${UI_REG}-${TAG} 
+docker push ${WEB_REG}-${TAG} 
+docker push ${UI_REG}-${TAG} 
+docker rmi bc_wildfire_web:${TAG} bc_wildfire_ui:${TAG}
