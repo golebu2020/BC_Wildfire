31705e6c11f411d660301cf6484c0124b0893485
God'swill Olebu <godswillolebu1234@gmail.com>
2023-08-30 22:06:36 -0700

    modififies settings.py'

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,6 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+os.environ['DJANGO_ALLOW_INVALID_HOSTS'] = '1'
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
