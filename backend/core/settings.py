75e84b1d3ddeafc0cd5228aa9783718ed9f68d50
God'swill Olebu <godswillolebu1234@gmail.com>
2023-08-31 22:55:27 -0700

    modifies settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-ropc*@04cbu4*59)0awml!=i@)5iue5ri!+43#b6#ahe-aotyb'
 
 # os.environ['DJANGO_ALLOW_INVALID_HOSTS'] = '1'
 
